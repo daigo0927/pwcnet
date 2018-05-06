4d99cd8b3d28cacbb5d3a078a5294ba458b3d9f7
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2018-05-06 20:04:36 +0900

    (I hope) complete modules

--- modules.py
+++ modules.py
@@ -18,17 +18,21 @@ class FeaturePyramidExtractor(object):
 
     def __init__(self, num_levels = 6, batch_norm = False, name = 'fp_extractor'):
         self.num_levels = num_levels
-        self.base_filters = 16
+        self.filters_list = [16, 32, 64, 96, 128, 192]
+        self.batch_norm = batch_norm
         self.name = name
 
-    def __call__(self, x):
+    def __call__(self, x, reuse = True):
         with tf.variable_scope(self.name) as vs:
+            if reuse:
+                vs.reuse_variables()
+                
             feature_pyramid = []
             for l in range(self.num_levels):
-                x = _conv_block(self.base_filters*2**l, (3, 3),
-                                (2, 2), batch_norm)(x)
-                x = _conv_block(self.base_filters*2**l, (3, 3),
-                                (1, 1), batch_norm)(x)
+                x = _conv_block(self.filters_list[l], (3, 3),
+                                (2, 2), self.batch_norm)(x)
+                x = _conv_block(self.filters_list[l], (3, 3),
+                                (1, 1), self.batch_norm)(x)
                 feature_pyramid.append(x)
 
             # return feature pyramid by ascent order
@@ -47,7 +51,7 @@ class WarpingLayer(object):
             grid_b, grid_y, grid_x = get_grid(x)
             warped_gy = tf.add(grid_y, flow[:,:,:,1]) # flow_y
             warped_gx = tf.add(grid_x, flow[:,:,:,0]) # flow_x
-            warped_indices = tf.stack([grid_b, warped_y, warped_x], axis = 3)
+            warped_indices = tf.stack([grid_b, warped_gy, warped_gx], axis = 3)
             
             warped_x = tf.gather_nd(x, warped_indices)
             return warped_x
@@ -59,85 +63,87 @@ class CostVolumeLayer(object):
         self.s_range = search_range
         self.name = name
 
-    def __call__(self, x, warped):
+    def __call__(self, x, warped, reuse = True):
         with tf.variable_scope(self.name) as vs:
             b, h, w, f = tf.unstack(tf.shape(x))
             cost_length = (2*self.s_range+1)**2
-            # cost = tf.Variable(tf.zeros(shape = (b, h, w, cost_length)),
-            #                    validate_shape = False)
-            cost = [tf.Variable(tf.zeros(shape = (b, h, w)),
-                                validate_shape = False) for _ in range(cost_length)]
+            cost_init = [tf.Variable(tf.zeros(shape = (b, h, w)),
+                                     validate_shape = False) for _ in range(cost_length)]
+            tf.variables_initializer(cost_init)
 
-            # cost = tf.Variable(tf.zeros((b, h, w)), validate_shape = False)
-            # cost_volume = [0]*2
-            # c_ = [0]*cost_length
-            # c_[0] = tf.assign(cost[:,:,:,0], tf.reduce_sum(warped*x, axis = 3))[:,:,:,0]
-            cost[0] = tf.assign(cost[0], tf.reduce_sum(warped*x, axis = 3))
-
-            # cost[1] = tf.assign(cost[:, 5:, 5:], tf.reduce_sum(warped[:, :-5, :-5]*x[:,5:,5:], axis = 3))
-            
-            # tf.assign(cost[:,:,:,0], tf.reduce_sum(warped*x, axis = 3))
+            cost = [0]*cost_length
+            cost[0] = tf.assign(cost_init[0], tf.reduce_sum(warped*x, axis = 3))
 
             I = 1
             for i in range(1, self.s_range+1):
-                cost[I] = tf.assign(cost[I][:,i:,:],
+                cost[I] = tf.assign(cost_init[I][:,i:,:],
                                     tf.reduce_sum(warped[:,:-i,:,:] * x[:,i:,:,:], axis = 3)); I+=1
-                cost[I] = tf.assign(cost[I][:,:-i,:],
+                cost[I] = tf.assign(cost_init[I][:,:-i,:],
                                     tf.reduce_sum(warped[:,i:,:,:] * x[:,:-i,:,:], axis = 3)); I+=1
-                cost[I] = tf.assign(cost[I][:,:,i:],
+                cost[I] = tf.assign(cost_init[I][:,:,i:],
                                     tf.reduce_sum(warped[:,:,:-i,:] * x[:,:,i:,:], axis = 3)); I+=1
-                cost[I] = tf.assign(cost[I][:,:,:-i],
+                cost[I] = tf.assign(cost_init[I][:,:,:-i],
                                     tf.reduce_sum(warped[:,:,i:,:] * x[:,:,:-i,:], axis = 3)); I+=1
 
                 for j in range(1, self.s_range+1):
-                    cost[I] = tf.assign(cost[I][:,i:,j:],
+                    cost[I] = tf.assign(cost_init[I][:,i:,j:],
                                         tf.reduce_sum(warped[:,:-i,:-j,:] * x[:,i:,j:,:], axis = 3)); I+=1
-                    cost[I] = tf.assign(cost[I][:,:-i,:-j],
+                    cost[I] = tf.assign(cost_init[I][:,:-i,:-j],
                                         tf.reduce_sum(warped[:,i:,j:,:] * x[:,:-i,:-j,:], axis = 3)); I+=1
-                    cost[I] = tf.assign(cost[I][:,i:,:-j],
+                    cost[I] = tf.assign(cost_init[I][:,i:,:-j],
                                         tf.reduce_sum(warped[:,:-i,j:,:] * x[:,i:,:-j,:], axis = 3)); I+=1
-                    cost[I] = tf.assign(cost[I][:,:-i,j:],
+                    cost[I] = tf.assign(cost_init[I][:,:-i,j:],
                                         tf.reduce_sum(warped[:,i:,:-j,:] * x[:,:-i,j:,:], axis = 3)); I+=1
-            
-            # I = 1
-            # for i in range(1, self.s_range+1):
-            #     c_[I] = tf.assign(cost[:,i:,:,I],
-            #                       tf.reduce_sum(warped[:,:-i,:,:] * x[:,i:,:,:], axis = 3))[:,:,:,I]; I+=1
-            #     c_[I] = tf.assign(cost[:,:-i,:,I],
-            #                       tf.reduce_sum(warped[:,i:,:,:] * x[:,:-i,:,:], axis = 3))[:,:,:,I]; I+=1
-            #     c_[I] = tf.assign(cost[:,:,i:,I],
-            #                       tf.reduce_sum(warped[:,:,:-i,:] * x[:,:,i:,:], axis = 3))[:,:,:,I]; I+=1
-            #     c_[I] = tf.assign(cost[:,:,:-i,I],
-            #                       tf.reduce_sum(warped[:,:,i:,:] * x[:,:,:-i,:], axis = 3))[:,:,:,I]; I+=1
-
-            #     for j in range(1, self.s_range+1):
-            #         c_[I] = tf.assign(cost[:,i:,j:,I],
-            #                           tf.reduce_sum(warped[:,:-i,:-j,:] * x[:,i:,j:,:], axis = 3))[:,:,:,I]; I+=1
-            #         c_[I] = tf.assign(cost[:,:-i,:-j,I],
-            #                           tf.reduce_sum(warped[:,i:,j:,:] * x[:,:-i,:-j,:], axis = 3))[:,:,:,I]; I+=1
-            #         c_[I] = tf.assign(cost[:,i:,:-j,I],
-            #                           tf.reduce_sum(warped[:,:-i,j:,:] * x[:,i:,:-j,:], axis = 3))[:,:,:,I]; I+=1
-            #         c_[I] = tf.assign(cost[:,:-i,j:,I],
-            #                           tf.reduce_sum(warped[:,i:,:-j,:] * x[:,:-i,j:,:], axis = 3))[:,:,:,I]; I+=1
-
-            # I = 1
-            # for i in range(1, self.s_range+1):
-            #     cost_volume[I] = tf.add(cost, tf.reduce_sum(warped[:,:-i,:,:] * x[:,i:,:,:], axis = 3)); I+=1
-            #     cost_volume[I] = tf.add(cost, tf.reduce_sum(warped[:,i:,:,:] * x[:,:-i,:,:], axis = 3)); I+=1
-            #     cost_volume[I] = tf.add(cost, tf.reduce_sum(warped[:,:,:-i,:] * x[:,:,i:,:], axis = 3)); I+=1
-            #     cost_volume[I] = tf.add(cost, tf.reduce_sum(warped[:,:,i:,:] * x[:,:,:-i,:], axis = 3)); I+=1
-                
-            #     for j in range(1, self.s_range+1):
-            #         cost_volume[I] = tf.add(cost, tf.reduce_sum(warped[:,:-i,:-j,:] * x[:,i:,j:,:], axis = 3)); I+=1
-            #         cost_volume[I] = tf.add(cost, tf.reduce_sum(warped[:,i:,j:,:] * x[:,:-i,:-j,:], axis = 3)); I+=1
-            #         cost_volume[I] = tf.add(cost, tf.reduce_sum(warped[:,:-i,j:,:] * x[:,i:,:-j,:], axis = 3)); I+=1
-            #         cost_volume[I] = tf.add(cost, tf.reduce_sum(warped[:,i:,:-j,:] * x[:,:-i,j:,:], axis = 3)); I+=1
 
             return tf.stack(cost, axis = 3) / cost_length
             
         
+class OpticalFlowEstimator(object):
 
-# class OpticalFlowEstimator(object):
+    def __init__(self, batch_norm, name = 'of_estimator'):
+        self.batch_norm = batch_norm
+        self.name = name
 
-#     def __init__(self, )
-            
+    def __call__(self, x, cost, flow, reuse = True):
+        with tf.variable_scope(self.name) as vs:
+            flow = tf.cast(flow, dtype = tf.float32)
+            x = tf.concat([x, cost, flow], axis = 3)
+            x = _conv_block(128, (3, 3), (1, 1), self.batch_norm)(x)
+            x = _conv_block(128, (3, 3), (1, 1), self.batch_norm)(x)
+            x = _conv_block(96, (3, 3), (1, 1), self.batch_norm)(x)
+            x = _conv_block(64, (3, 3), (1, 1), self.batch_norm)(x)
+            feature = _conv_block(32, (3, 3), (1, 1), self.batch_norm)(x)
+            flow = tf.layers.Conv2D(2, (3, 3), (1, 1), padding = 'same')(feature)
+
+            return feature, flow # x:processed feature, w:processed flow
+
+    
+class ContextNetwork(object):
+
+    def __init__(self, name = 'context'):
+        self.name = name
+
+    def __call__(self, feature, flow, reuse = True):
+        with tf.variable_scope(self.name) as vs:
+            x = tf.concat([feature, flow], axis = 3)
+            x = tf.layers.Conv2D(128, (3, 3), (1, 1),'same',
+                                 dilation_rate = (1, 1))(x)
+            x = tf.nn.leaky_relu(x, 0.2)
+            x = tf.layers.Conv2D(128, (3, 3), (1, 1),'same',
+                                 dilation_rate = (2, 2))(x)
+            x = tf.nn.leaky_relu(x, 0.2)
+            x = tf.layers.Conv2D(128, (3, 3), (1, 1),'same',
+                                 dilation_rate = (4, 4))(x)
+            x = tf.nn.leaky_relu(x, 0.2)
+            x = tf.layers.Conv2D(96, (3, 3), (1, 1),'same',
+                                 dilation_rate = (8, 8))(x)
+            x = tf.nn.leaky_relu(x, 0.2)
+            x = tf.layers.Conv2D(64, (3, 3), (1, 1),'same',
+                                 dilation_rate = (16, 16))(x)
+            x = tf.nn.leaky_relu(x, 0.2)
+            x = tf.layers.Conv2D(32, (3, 3), (1, 1),'same',
+                                 dilation_rate = (1, 1))(x)
+            x = tf.nn.leaky_relu(x, 0.2)
+            x = tf.layers.Conv2D(2, (3, 3), (1, 1),'same',
+                                 dilation_rate = (1, 1))(x)
+            return x+flow
