75510491f51b2c76c487aa8e884e26c19067de6e
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2018-05-10 01:49:50 +0900

    remove bug (identical name of diffrent variables)

--- losses.py
+++ losses.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import pdb 
 
 def L1loss(x, y):
     return tf.reduce_mean(tf.norm(x-y, ord = 1, axis = 3))
@@ -14,21 +15,25 @@ def multiscale_loss(flows_gt, flows_pyramid,
                     weights, name = 'multiscale_loss'):
     with tf.name_scope(name) as ns:
         _, H, W, _ = tf.unstack(tf.shape(flows_gt))
-        loss, epe = 0, 0
+        # loss, epe = 0., 0.
+        loss, epe = 0., 0.
         loss_levels, epe_levels = [], []
-
+        
         # processing from coarce-to-fine level
-        for l, (w, fs) in enumerate(zip(weights, flows_pyramid)):
+        for l, (weight, fs) in enumerate(zip(weights, flows_pyramid)):
             _, h, w, _ = tf.unstack(tf.shape(fs))
-            fs_gt = tf.image.resize_bilinear(flows_gt, (h, w))/(H/h)
+
+            fs_gt = tf.image.resize_bilinear(flows_gt, (h, w))\
+                    /tf.cast(H/h, dtype = tf.float32)
             
             loss_level = L2loss(fs_gt, fs)
-            loss += w * loss_level
+            loss += weight * loss_level
             loss_levels.append(loss_level)
 
-            epe = EPE(fs_gt, fs)
-            epe_levels.append(epe)
-
+            epe_level = EPE(fs_gt, fs)
+            epe += epe_level
+            epe_levels.append(epe_level)
+        
         return loss, epe, loss_levels, epe_levels
 
 def multirobust_loss(flows_gt, flows_pyramid,
