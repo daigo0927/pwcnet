7a5b641c5221018a58fecf56178540e437aebaa2
Daigo Hirooka <daigo@gorgon.murata.eb.waseda.ac.jp>
2018-05-11 01:42:48 +0900

    add context-network utility

--- model.py
+++ model.py
@@ -7,26 +7,32 @@ class PWCNet(object):
 
     def __init__(self, num_levels = 6, search_range = 4,
                  output_level = 4, batch_norm = False,
-                 name = 'pwcnet'):
+                 context = 'all', name = 'pwcnet'):
         self.num_levels = num_levels
         self.s_range = search_range
         self.output_level = output_level
         self.batch_norm = batch_norm
+        self.context = context
         self.name = name
 
         self.fp_extractor = FeaturePyramidExtractor(self.num_levels, batch_norm)
         self.warp_layer = WarpingLayer()
         self.cv_layer = CostVolumeLayer(search_range)
         self.of_estimators = [OpticalFlowEstimator(self.batch_norm,
-                                                   name = f'of_level{l}')\
+                                                   name = f'optflow_{l}')\
                               for l in range(self.num_levels)]
-        self.context = ContextNetwork()
+        # self.contexts = ContextNetwork()
+        assert self.context in ['all', 'final'], 'context argument should be all/final'
+        if self.context is 'all':
+            self.context_nets = [ContextNetwork(name = f'context_{l}')\
+                                 for l in range(self.num_levels)]
+        else:
+            self.context_net = ContextNetwork(name = 'context')
 
     def __call__(self, images_0, images_1):
         with tf.variable_scope(self.name) as vs:
 
-            reuse = False
-            pyramid_0 = self.fp_extractor(images_0, reuse = reuse) + [images_0]
+            pyramid_0 = self.fp_extractor(images_0, reuse = False) + [images_0]
             pyramid_1 = self.fp_extractor(images_1) + [images_1]
 
             flows = []
@@ -44,15 +50,18 @@ class PWCNet(object):
 
                 # warping -> costvolume -> optical flow estimation
                 feature_1_warped = self.warp_layer(feature_1, flow)
-                cost = self.cv_layer(feature_0, feature_1_warped, reuse = reuse)
+                cost = self.cv_layer(feature_0, feature_1_warped)
                 feature, flow = self.of_estimators[l](feature_0, cost, flow)
 
-                # context considering process
-                flow = self.context(feature, flow, reuse = reuse)
+                # context considering process all/final
+                if self.context is 'all':
+                    flow = self.context_nets[l](feature, flow)
+                elif l == self.output_level: 
+                    flow = self.context_net(feature, flow)
 
                 flows.append(flow)
                 summaries['feature_1_warps'].append(feature_1_warped)
-                reuse = True
+                
                 # stop processing at the defined level
                 if l == self.output_level:
                     upscale = 2**(self.num_levels - self.output_level)
