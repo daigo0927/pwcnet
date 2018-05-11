c5acfc6d9f6df29addc198fbaa5e53b1ce0f3cce
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2018-05-11 20:41:08 +0900

    add mutiscale-robust loss

--- losses.py
+++ losses.py
@@ -40,12 +40,19 @@ def multirobust_loss(flows_gt, flows_pyramid,
                      weights, epsilon = 0.01,
                      q = 0.4, name = 'multirobust_loss'):
     with tf.name_scope(name) as ns:
-        for l, (w, fs) in enumerate(zip(weights, flows_pyramid)):
+        _, H, W, _ = tf.unstack(tf.shape(flows_gt))
+        # loss, epe = 0., 0.
+        loss, epe = 0., 0.
+        loss_levels, epe_levels = [], []
+        
+        for l, (weight, fs) in enumerate(zip(weights, flows_pyramid)):
             _, h, w, _ = tf.unstack(tf.shape(fs))
-            fs_gt = tf.image.resize_bilinear(flows_gt, (h, w))/(H/h)
             
-            loss_level = (L1loss(fs_gt, fs) + epsilon)**q
-            loss += w * loss_level
+            fs_gt = tf.image.resize_bilinear(flows_gt, (h, w))\
+                    /tf.cast(H/h, dtype = tf.float32)
+            
+            loss_level = L1loss(fs_gt, fs)
+            loss += weight * (loss_level+epsilon)**q
             loss_levels.append(loss_level)
 
             epe = EPE(fs_gt, fs)
