fae5633029667d203f63f775a9628393d850c833
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2018-08-27 02:15:49 +0900

    add PWCDCNet model

--- model.py
+++ model.py
@@ -72,3 +72,59 @@ class PWCNet(object):
     @property
     def vars(self):
         return [var for var in tf.global_variables() if self.name in var.name]
+
+
+class PWCDCNet(object):
+    def __init__(self, num_levels = 6, search_range = 4, output_level = 4,
+                 name = 'pwcdcnet'):
+        self.num_levels = num_levels
+        self.s_range = search_range
+        assert output_level < num_levels, 'Should set output_level < num_levels'
+        self.output_level = output_level
+        self.name = name
+
+        self.fp_extractor = FeaturePyramidExtractor_custom(self.num_levels)
+        self.warp_layer = WarpingLayer()
+        self.cv_layer = CostVolumeLayer(search_range)
+        self.of_estimators = [OpticalFlowEstimator_custom(name = f'optflow_{l}')\
+                              for l in range(self.num_levels)]
+        self.context = ContextNetwork(name = 'context')
+        # Upscale factors from deep -> shallow level
+        self.scales = [None, 0.625, 1.25, 2.5, 5.0, 10., 20.]
+
+    def __call__(self, images_0, images_1):
+        with tf.variable_scope(self.name) as vs:
+            pyramid_0 = self.fp_extractor(images_0, reuse = 0)
+            pyramid_1 = self.fp_extractor(images_1)
+
+            flows = []
+            for l, (feature_0, feature_1) in enumerate(zip(pyramid_0, pyramid_1)):
+                print(f'Level {l}')
+
+                # Warping operation
+                if l == 0:
+                    feature_1_warped = feature_1
+                else:
+                    feature_1_warped = self.warp_layer(feature_1, flow_up*self.scales[l])
+
+                cost = self.cv_layer(feature_0, feature_1_warped)
+                if l == 0:
+                    flow, flow_up, feature_up = self.of_estimators[l](cost)
+                elif l < self.output_level:
+                    flow, flow_up, feature_up \
+                        = self.of_estimators[l](cost, feature_0, flow_up, feature_up)
+                else:
+                    # At output level
+                    feature, flow = self.of_estimators[l](cost, feature_0, flow_up, feature_up,
+                                                          is_output = True)
+                    flow = self.context(feature, flow)
+                    flows.append(flow)
+                    return flows
+
+                flows.append(flow)
+
+    @property
+    def vars(self):
+        return [var for var in tf.global_variables() if self.name in var.name]
+
+                
